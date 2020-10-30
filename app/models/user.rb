ab1a187eaa42fc4fc82f11e58ed6000d6eacffce
Fanny Rostaing <fanny@coffretis.com>
2020-10-30 09:26:17 +0100

    Add dotenv - Protect my secret data in .env file

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
+
+         def self.from_omniauth(auth)
+           self.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+             user.email = auth.info.email
+             user.pseudo = auth.info.name
+             # dowloaded_image = auth.info.image
+             # cloudinary_image = Cloudinary::Uploader.upload(dowloaded_image, public_id: auth.uid)
+             # cloudinary_url = cl_image_tag(auth.uid)
+             # user.photo.attach(io: cloudinary_url, filename: auth.uid)
+             user.provider = auth.provider
+             user.uid = auth.uid
+             user.password = Devise.friendly_token[0,20]
+           end
+         end
 end
